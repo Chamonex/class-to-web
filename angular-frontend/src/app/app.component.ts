d5c6566cf2beb1f99554b10ee50753bbbca670d5
Pedro Chamone <chamonepe@gmail.com>
2025-01-31 11:47:43 -0300

    feat: frontend permite inserir arquivos

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -1,11 +1,51 @@
 import { Component } from '@angular/core';
+import { NgxFileDropEntry, FileSystemFileEntry, FileSystemDirectoryEntry, NgxFileDropModule} from 'ngx-file-drop';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [NgxFileDropModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-frontend';
+  public files: NgxFileDropEntry[] = [];
+  
+  public dropped(files: NgxFileDropEntry[]) {
+    this.files = files;
+    for (const droppedFile of files) {
+
+      // Is it a file?
+      if (droppedFile.fileEntry.isFile) {
+        const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
+        fileEntry.file((file: File) => {
+
+          // Here you can access the real file
+          console.log("AAA", droppedFile.relativePath, "BBB", file);
+
+          // You could upload it like this:
+          const formData = new FormData()
+          formData.append('logo', file, droppedFile.relativePath)
+
+          // this.http.post('https://mybackend.com/api/upload/sanitize-and-save-logo', formData, { headers: headers, responseType: 'blob' })
+          // .subscribe(data => {
+          //   // Sanitized logo returned from backend
+          // })
+ 
+        });
+      } else {
+        // It was a directory (empty directories are added, otherwise only files)
+        const fileEntry = droppedFile.fileEntry as FileSystemDirectoryEntry;
+        console.log(droppedFile.relativePath, fileEntry);
+      }
+    }
+  }
+
+  public fileOver(event: any){
+    console.log(event);
+  }
+
+  public fileLeave(event: any){
+    console.log(event);
+  }
 }
